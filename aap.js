a83af797b310639006e21743d3fa5bdc5c63098a
SabatJamil <155986467+SabatJamil@users.noreply.github.com>
2024-01-22 18:44:43 +0500

    Add files via upload

---
+++ aap.js
@@ -0,0 +1,11 @@
+let width = 10;
+let length = 5;
+var area = width*length;
+console.log("Area of rectangle = " , area);
+
+
+
+let userName = prompt("What is your name","Guest");
+console.log(userName);
+alert(userName)
+alert("you sre welcome")
---
+++ app.js
@@ -0,0 +1,8 @@
+// Alert Message
+alert("Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus architecto repudiandae necessitatibus utquidem. Excepturi asperiores quae non nemo atque explicabo incidunt voluptatibus aliquid assumenda?")
+
+var age = prompt("What is your age", "");
+var student = prompt("Are you a student?", "Yes/No");
+console.log(age);
+console.log(student);
+
